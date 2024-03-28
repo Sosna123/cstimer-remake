c63268307f21d0d8644ded38a754cef2f289dc23
Sosna123 <olek.arez@gmail.com>
2024-03-28 15:29:35 +0100

    project setup

---
+++ src/views/Timer.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "timer",
+    setup() {},
+});
+</script>
+
+<style></style>
